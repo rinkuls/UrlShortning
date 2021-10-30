103aa185cbadf44d9e25f63b318709825787093f
Rinkuls <rinkul.sharma123@gmail.com>
2021-10-30 22:49:20 +0200

    new testyuio

--- src/main/java/com/codingTest/urlShorteningService/repo/UrlRepository.java
+++ src/main/java/com/codingTest/urlShorteningService/repo/UrlRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
-    
+
     public Url findByShortenUrl(String shortenUrl);
 }
